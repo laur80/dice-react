efc6986edab769847e937835990e33ff685fef0c
laur80 <laur80raul@gmail.com>
2020-10-20 16:31:14 +0300

    Updates

---
+++ precache-manifest.c469bb2b131545e9e31c0c6510f5acf8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e562dec6ddd9cfa1a5d4dc98c0d7c71f",
+    "url": "/dice-react/index.html"
+  },
+  {
+    "revision": "335ff47b27245758acb3",
+    "url": "/dice-react/static/css/main.b42f7550.chunk.css"
+  },
+  {
+    "revision": "efebbff08dfc25d9c1ff",
+    "url": "/dice-react/static/js/2.aaeeb12a.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/dice-react/static/js/2.aaeeb12a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "335ff47b27245758acb3",
+    "url": "/dice-react/static/js/main.9d3b6f20.chunk.js"
+  },
+  {
+    "revision": "8a934bb442e1a3927235",
+    "url": "/dice-react/static/js/runtime-main.2a40b685.js"
+  }
+]);
